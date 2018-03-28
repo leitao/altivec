2a4d3e5582bc461eba7d8456a0c73131e73b6a77
Breno Leitao <breno.leitao@gmail.com>
2018-03-28 10:47:51 -0400

    Core files

---
+++ test.c
@@ -0,0 +1,42 @@
+#include <altivec.h>
+#include <stdio.h>
+
+void print_vec_char(char *s, vector signed char _data){
+    int i;
+
+    printf("%s\t:", s);
+    for (i = 0 ; i <= 15; i++)
+        printf("%3d ", vec_extract(_data, i));
+    printf("\n");
+}
+
+void print_vec_long(char *s, vector signed long int _data){
+    int i;
+
+    printf("%s\t:", s);
+    for (i = 0 ; i <= 1; i++)
+        printf("%ld ", vec_extract(_data, i));
+    printf("\n");
+}
+
+int main(){
+    vector signed long int output;
+    signed char x;
+    vector signed char _data;
+    const vector signed char bits = {120, 112, 104, 96, 88, 80, 72, 64, 56, 48, 40, 32, 24, 16, 8, 0};
+
+
+    // Initialize the vector _data with the same values
+    _data = vec_splat_s8(-0x1);
+
+    print_vec_char("_data", _data);
+    print_vec_char("bits", bits);
+
+    output = vec_vbpermq(_data, bits);
+    print_vec_long("output", output);
+
+    x = vec_extract(output, 1);
+
+    printf("Output = %x\n", x);
+
+}
